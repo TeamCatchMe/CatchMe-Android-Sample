0bb3240dc40584a45e6f3047a48a583a1f64b947
seojin kim <001106ksj@gmail.com>
2021-07-09 17:20:35 +0900

    [feature/calendar] ui 작업
    * 상단 월화수목금토일 그리드
    * 상단 연-월 뜨도록
    * 상단 화살표 작동하도록

--- app/src/main/java/com/teamcatchme/calendar/CalendarActivity.kt
+++ app/src/main/java/com/teamcatchme/calendar/CalendarActivity.kt
@@ -3,10 +3,23 @@ package com.teamcatchme.calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
 import com.teamcatchme.catchmesample.databinding.ActivityCalendarBinding
 import java.time.LocalDate
 
 class CalendarActivity : AppCompatActivity() {
+    companion object {
+        const val RIGHT = 1
+        const val LEFT = -1
+    }
+
+    private fun onArrowClicked(direction: Int) {
+        binding.viewpagerCalendar.setCurrentItem(
+            binding.viewpagerCalendar.currentItem + direction,
+            true
+        )
+    }
+
     lateinit var binding: ActivityCalendarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +28,24 @@ class CalendarActivity : AppCompatActivity() {
 
         val monthAdapter = MonthAdapter()
         val dateTime = LocalDate.now()
+        val currentYear = dateTime.year
         val currentMonth = dateTime.month.value
         Log.d("태그", "set current month ${36 + currentMonth}")
         binding.viewpagerCalendar.adapter = monthAdapter
         binding.viewpagerCalendar.currentItem = 35 + currentMonth
+        updateTxtUI(currentYear, currentMonth)
+        binding.viewpagerCalendar.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                updateTxtUI(currentYear - 3 + (position / 12), (position % 12) + 1)
+            }
+        })
+        binding.btnReportDialogMoveLeft.setOnClickListener { onArrowClicked(LEFT) }
+        binding.btnReportDialogMoveRight.setOnClickListener { onArrowClicked(RIGHT) }
+    }
 
+    fun updateTxtUI(year: Int, month: Int) {
+        val txtYearMonth = "$year - $month"
+        binding.txtCalendarYearmonth.text = txtYearMonth
     }
 }
